30bafc104b409ab945702b9106e97aaf2e8ddefa
John Oncher <johnoncher@gmail.com>
2019-03-04 16:38:15 -0500

    added the searched books to the page

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ document.addEventListener('DOMContentLoaded', function(event){
 
 })
 
+
+//get data for searching books
 function getBooksData(searchInput){
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`)
   .then(resp => resp.json())
-  .then(search => console.log(search))
+  .then(search => renderBooksToPage(search.items))
 }
+
+function renderBooksToPage(searchData) {
+  for (bookData of searchData) {
+    renderSingleBookToPage(bookData.volumeInfo)
+  }
+}
+
+function renderSingleBookToPage(bookData){
+  const searchResultsDiv = document.querySelector('#search-books')
+  const bookDiv = document.createElement('div')
+  bookDiv.innerHTML =
+  `
+  <img src=${bookData.imageLinks.thumbnail}>
+  <h4> ${bookData.title} </h4>
+  <button> Add Book to Bookshelf </button>
+  `
+  bookDiv.querySelector('button').addEventListener('click', handleClick)
+  searchResultsDiv.append(bookDiv)
+}
+
+
+//
