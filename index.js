53fa7752dd9764bd9320bb723fe5fff8423e1d81
John Oncher <johnoncher@gmail.com>
2019-03-05 12:00:14 -0500

    search for books, render results to page, and be able to post them to api

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(event){
-  console.log("we in this shit")
 
 })
 
@@ -26,9 +25,47 @@ function renderSingleBookToPage(bookData){
   <h4> ${bookData.title} </h4>
   <button> Add Book to Bookshelf </button>
   `
-  bookDiv.querySelector('button').addEventListener('click', handleClick)
+  bookDiv.querySelector('button').addEventListener('click', function(event) {
+      handleClick(event, bookData)
+    })
+
   searchResultsDiv.append(bookDiv)
 }
 
+function handleClick(e, bookData) {
+  const bookObj = {
+    'title': bookData.title,
+    'author': bookData.authors[0],
+    'publisher': bookData.publisher,
+    'description': bookData.description,
+    'isbn_10': bookData.industryIdentifiers[1].identifier,
+    'page_count': bookData.pageCount,
+    'average_review': bookData.averageRating,
+    'thumbnail': bookData.imageLinks.thumbnail,
+    'info_link': bookData.infoLink
+  }
 
-//
+  postNewBook(bookObj)
+
+  //find or create by into database
+  //add to our book list
+  //strike through button and say added to book list
+  //
+}
+
+function postNewBook(bookObj){
+  fetch('http://localhost:3000/books',{
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(bookObj)
+  })
+}
+
+function renderMyBooks(){
+  fetch('https://localhost3000.com/books')
+  .then(resp => resp.json())
+  .then(books => addBooksToList(books))
+}
